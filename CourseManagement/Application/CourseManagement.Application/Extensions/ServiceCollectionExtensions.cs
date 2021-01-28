5cce795f1f8a100f9c156d76159b4f1115793524
yurii-didyk <55990163+yuriididyk@users.noreply.github.com>
2021-01-28 21:12:33 +0200

    Added some required NuGet packages, created basic folders structure

---
+++ CourseManagement/Application/CourseManagement.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CourseManagement.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    { 
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
