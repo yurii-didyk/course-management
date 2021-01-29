d0b02ab1090fe2eeb5c3f445cd47edec2b866af9
yurii-didyk <55990163+yuriididyk@users.noreply.github.com>
2021-01-29 21:35:04 +0200

    Added DomainSpecific helpers

--- CourseManagement/Domain/CourseManagement.Domain/Entities/Course.cs
+++ CourseManagement/Domain/CourseManagement.Domain/Entities/Course.cs
@@ -1,6 +1,7 @@
 ﻿using CourseManagement.Domain.Abstractions;
 using CourseManagement.Domain.Exceptions;
 using CourseManagement.Domain.Exceptions.Enums;
+using CourseManagement.Domain.Helpers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -18,8 +19,9 @@ namespace CourseManagement.Domain.Entities
         public void SetTime(DateTime startTime, DateTime endTime)
         {
             var isStartTimeBeforeEndTime = startTime <= endTime;
-            var isWeekDayCorrect = AllowedDaysOfWeek.Contains(startTime.DayOfWeek) && AllowedDaysOfWeek.Contains(endTime.DayOfWeek);
-            var isTimeCorrect = IsWorkTime(startTime) && IsWorkTime(endTime);
+            var isWeekDayCorrect = DateHelper.AllowedDaysOfWeek.Contains(startTime.DayOfWeek) &&
+                DateHelper.AllowedDaysOfWeek.Contains(endTime.DayOfWeek);
+            var isTimeCorrect = DateHelper.IsWorkTime(startTime) && DateHelper.IsWorkTime(endTime);
 
             if (isStartTimeBeforeEndTime && isWeekDayCorrect && isTimeCorrect)
             {
@@ -29,34 +31,7 @@ namespace CourseManagement.Domain.Entities
                 return;
             }
 
-            throw GetException(isWeekDayCorrect, isTimeCorrect);
-        }
-
-        [NotMapped]
-        private static List<DayOfWeek> AllowedDaysOfWeek = new List<DayOfWeek>() 
-        {   DayOfWeek.Monday,
-            DayOfWeek.Tuesday,
-            DayOfWeek.Wednesday,
-            DayOfWeek.Thursday,
-            DayOfWeek.Friday 
-        };
-
-        private static bool IsWorkTime(DateTime dt) => dt.Hour >= 9 && dt.Hour <= 17; 
-
-        private static Exception GetException(bool isWeekDayCorrect, bool isTimeCorrect)
-        {
-            var reason = ValidationFailed.FinishedEarlierThanStarted;
-
-            if (!isWeekDayCorrect)
-            {
-                reason = ValidationFailed.DayOfWeek;
-            }
-            else if (!isTimeCorrect)
-            {
-                reason = ValidationFailed.Time;
-            }
-
-            return new CourseValidationException(reason);
+            throw ExceptionHelper.GetException(isWeekDayCorrect, isTimeCorrect);
         }
     }
 }
---
+++ CourseManagement/Domain/CourseManagement.Domain/Helpers/ExceptionHelper.cs
@@ -0,0 +1,27 @@
+﻿using CourseManagement.Domain.Exceptions;
+using CourseManagement.Domain.Exceptions.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseManagement.Domain.Helpers
+{
+    public static class ExceptionHelper
+    {
+        public static Exception GetException(bool isWeekDayCorrect, bool isTimeCorrect)
+        {
+            var reason = ValidationFailed.FinishedEarlierThanStarted;
+
+            if (!isWeekDayCorrect)
+            {
+                reason = ValidationFailed.DayOfWeek;
+            }
+            else if (!isTimeCorrect)
+            {
+                reason = ValidationFailed.Time;
+            }
+
+            return new CourseValidationException(reason);
+        }
+    }
+}
